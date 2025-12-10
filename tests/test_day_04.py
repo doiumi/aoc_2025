8633bba048fd96d20614688a97d8dfc8d34aaebf
doiumi <iustinian.dominte@gmail.com>
2025-12-10 13:57:37 +0200

    Add day 04 solver stub and tests

---
+++ tests/test_day_04.py
@@ -0,0 +1,13 @@
+import pytest
+
+from aoc_2025.solutions import day_04
+
+
+def test_part1_placeholder():
+    with pytest.raises(NotImplementedError):
+        day_04.solve_part1("test")
+
+
+def test_part2_placeholder():
+    with pytest.raises(NotImplementedError):
+        day_04.solve_part2("test")
